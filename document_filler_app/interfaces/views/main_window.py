ddeb51e26f58e4667fa330fae551287e58b1c743
Klayton COD.ias <65176457+Ton-Chyod-s@users.noreply.github.com>
2025-05-03 10:26:58 -0400

    feat: Atualização do título da janela e ajustar tamanho no MainWindow

--- document_filler_app/interfaces/views/main_window.py
+++ document_filler_app/interfaces/views/main_window.py
@@ -4,14 +4,16 @@ from PySide6.QtWidgets import QMainWindow, QPushButton, QLabel, QWidget, QVBoxLa
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("App PySide6")
-        self.setFixedSize(300, 200)
+        self.setWindowTitle("Preenchimento de Documento  ")
+        self.adjustSize()
+        # self.resize(300, 200)
 
         self.central_widget = QWidget()
         self.setCentralWidget(self.central_widget)
 
         self.label = QLabel("Clique no botão")
         self.button = QPushButton("Clique aqui")
+        self.button.setFixedSize(320, 25)
 
         layout = QVBoxLayout()
         layout.addWidget(self.label)
